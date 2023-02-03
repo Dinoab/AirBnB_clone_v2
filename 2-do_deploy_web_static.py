0fc8d64d7121c44bca74316e0a866dcfa1e543e9
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2023-02-03 06:59:12 -0800

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['52.205.193.52', '54.197.21.122']
 
 
 def do_deploy(archive_path):
